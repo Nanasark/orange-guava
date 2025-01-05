c57c1b577e5eb0601615d1c6b2f3a01c37b0d12a
Nanasark <danielnyamekyesarkodie@gmail.com>
2025-01-05 18:42:33 +0000

    seris

--- src/components/connectWallet.tsx
+++ src/components/connectWallet.tsx
@@ -1,5 +1,5 @@
 import { client } from "@/app/client";
-import { polygon} from "thirdweb/chains";
+import { polygon } from "thirdweb/chains";
 import { ConnectButton } from "thirdweb/react";
 
 export default function ConnectWallet() {
@@ -7,9 +7,9 @@ export default function ConnectWallet() {
     <>
       <ConnectButton
         client={client}
-        chain={polygonAmoy}
+        chain={polygon}
         supportedTokens={{
-          : [
+          137: [
             {
               name: "USD Coin",
               address: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
