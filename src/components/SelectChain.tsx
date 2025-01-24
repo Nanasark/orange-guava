e5928ef414ec625649a8b43d20e4c900c0cd3a0a
Nanasark <danielnyamekyesarkodie@gmail.com>
2025-01-24 15:39:58 +0000

    kjk

--- src/components/SelectChain.tsx
+++ src/components/SelectChain.tsx
@@ -1,7 +1,7 @@
+"use client";
 import React, { useState } from "react";
 import { chain, chainId } from "@/app/chain";
 import { ContractAddress, USDCAddress } from "@/utils/getChainAddresses";
-import Image from "next/image";
 
 let currentSelectedChain = chain("CELO");
 let currentSelectedChainId = chainId("CELO");
@@ -24,7 +24,6 @@ export function getUSDCAddress() {
 }
 
 export default function SelectChain() {
-
   const [activeChain, setActiveChain] = useState("CELO");
 
   const handleChainChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -48,7 +47,6 @@ export default function SelectChain() {
         className="bg-blue-400 text-white text-sm rounded-md border-none px-3 py-1.5 pr-8 focus:outline-none focus:ring-2 focus:ring-blue-400 hover:bg-blue-700 transition-colors duration-200 appearance-none cursor-pointer"
       >
         <option value="CELO" className="flex gap-2">
-          <Image alt="" src={"/celo.png"} width={20} height={20} />
           <p>CELO</p>
         </option>
         <option value="POLYGON">POLYGON</option>
